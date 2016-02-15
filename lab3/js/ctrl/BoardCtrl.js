a58614cddb669a3712e6ec241cb86f61b5f10182
Calle <cstenson@kth.se>
2016-02-15 16:07:45 +0100

    all

--- lab3/js/ctrl/BoardCtrl.js
+++ lab3/js/ctrl/BoardCtrl.js
@@ -2,34 +2,61 @@ var BoardCtrl = function (view, model) {
 	var _this = this;
 
 	this.update = function() {
-		$(".sea" ).click(function() {
-			//console.log(this.id);
-			//model.test(if hit);
-			var id = this.id.split(":")
-			var row = id[0];
-			var col = id[1];
 
-			model.guess(row, col);
+		if (!model.gameStarted){
+			view.printArray(model.currentPlayer.grid);
+		} else {
 			//console.log(model.player1);
-
 			view.printArray(model.currentPlayer.guesses);
-			_this.update();
+		}
+
+		if (!model.guessMade) {
+			$(".sea" ).click(function() {
+				//console.log(this.id);
+				//model.test(if hit);
+				var id = this.id.split(":")
+				var x = parseInt(id[0]);
+				var y = parseInt(id[1]);
+
+				if(!model.gameStarted){
+					model.addBoat(x, y,2, "v");
+				}else{
+					model.guess(x, y);
+				}
+			});
+		}
+		if (model.winner !== null) {
+			alert("You have won!");
+		}
+
+		view.doneButton.click( function() {
+			console.log("doneclick)");
+			//console.log(model.currentPlayer.boats.length);
+			if (!model.gameStarted && model.player1.boats.length >= 1 && model.player2.boats.length >= 1){
+				model.startGame();
+				model.switchPlayer();
+			} else if (!model.gameStarted && (model.currentPlayer === model.player1) && model.currentPlayer.boats.length >= 1) {
+				model.switchPlayer();
+			} else if (model.gameStarted && (model.winner === null)) {
+				model.switchPlayer();
+			} else if (model.gameStarted && (model.winner !== null)) {
+				location.reload();
+			}
+
+			view.doneButton.unbind().click(function() { console.log("unbind")});
+
 		});
+
+		view.currentPlayer.html("Player "+model.currentPlayer.playerNumber);
 	};
 
 
-	this.makeOnclick = function(){
-
-	}
-
 	//initialize from the start
-	if(!model.gameStarted){
-		view.printArray(model.currentPlayer.grid);
-		this.update();
-	}else{
-		view.printArray(model.currentPlayer.guesses);
-		this.update();
-	}
+
+	//console.log("Game: "+ model.gameStarted);
+
+	
+	this.update();
 	
 
 	//Add observer
--- lab3/js/model/model.js
+++ lab3/js/model/model.js
@@ -1,12 +1,24 @@
 var BattleshipGame = function() {
 	var _this = this;
+	var gridSize = 9;
 	this.observers = [];
 	this.gameStarted = false;
+	this.guessMade = false;
+	this.winner = null;
 
-	function notifyObservers() {
+	this.notifyObservers = function() {
 		for (i in this.observers) {
 			this.observers[i].update();
 		}
+	}
+
+	this.startGame = function() {
+		if (this.gameStarted === false) {
+			this.gameStarted = true;
+		}
+
+		_this.notifyObservers();
+
 	}
 
 	this.otherPlayer = function() {
@@ -17,23 +29,58 @@ var BattleshipGame = function() {
 		}
 	}
 
+	this.switchPlayer = function() {
+		console.log("Switch player");
+		if (_this.currentPlayer === _this.player1) {
+			_this.currentPlayer = _this.player2;
+		} else {
+			_this.currentPlayer = _this.player1;
+		}
+
+		_this.guessMade = false;
+
+		_this.notifyObservers();
+	}
+
 	this.addObserver = function(controller) {
 		this.observers.push(controller);
 	}
 
 	this.guess = function(x, y) {
-		if (this.otherPlayer().grid[x][y] !== undefined) {
-			this.otherPlayer().grid[x][y].hit();
-			this.currentPlayer.guesses[x][y] = "hit";
+		if (this.otherPlayer().grid[y][x] !== undefined) {
+			this.otherPlayer().grid[y][x].hit();
+			this.currentPlayer.guesses[y][x] = "hit";
+
+			if (this.checkIfWinner()) {
+				this.winner = this.currentPlayer;
+				console.log("You have won");
+			}
 		} else {
-			this.currentPlayer.guesses[x][y] = "empty";
+			this.currentPlayer.guesses[y][x] = "empty";
 		}
 
-		notifyObservers();
+		_this.guessMade = true;
+		_this.notifyObservers();
 	}
 
-	this.player1 = new Player(1);
-	this.player2 = new Player(2);
+	this.checkIfWinner = function() {
+		for (var i in this.otherPlayer().boats) {
+			if (!this.otherPlayer().boats[i].isDestroyed) {
+				return false;
+			}
+		}
+
+		return true; // if there has been no encounter of a boat that has not been destroyed, all boats have been destroyed
+	}
+
+	this.addBoat = function(x ,y ,length, rotation){
+		//control if valid input
+		_this.currentPlayer.addBoat(x ,y ,length, rotation);
+		_this.notifyObservers();
+	}
+
+	this.player1 = new Player(1, gridSize);
+	this.player2 = new Player(2, gridSize);
 	this.currentPlayer = this.player1;
 
 
@@ -46,38 +93,37 @@ var Grid = function() {
 
 }
 
-var Player = function(playerNumber) {
+var Player = function(playerNumber, gridSize) {
 	var _this = this;
 	this.playerNumber = playerNumber;
 	this.boats = [];
 	this.grid = [];
 	this.guesses = [];
 
-	for (var i = 0; i < 9; i ++) {
-		this.grid.push(new Array(9));
+	for (var i = 0; i < gridSize; i ++) {
+		this.grid.push(new Array(gridSize));
 	}
 
-	for (var i = 0; i < 9; i ++) {
-		this.guesses.push(new Array(9));
+	for (var i = 0; i < gridSize; i ++) {
+		this.guesses.push(new Array(gridSize));
 	}
 
 	//console.log(this.grid);
 	//console.log(this.guesses[0][0] === undefined);
 
-	this.addBoat = function(length, rotation, x, y) {
+	this.addBoat = function(x ,y , length, rotation) {
 		_this.boats.push(new Boat(length, rotation));
+		console.log("add boat at: "+x +":"+y+":"+length+":"+rotation);
 
-		if (length === "h") {
-			for (var i = x; i <= length; i ++) {
-				_this.grid[i][y] = _this.boats[_this.boats.length - 1];
+		if (rotation == "h") {
+			for (i = x; i < x + length; i++) {
+				_this.grid[y][i] = _this.boats[_this.boats.length - 1];
 			}
 		} else if (rotation === "v") {
-			for (var j = y; j <= length; j ++) {
-				_this.grid[x][j] = _this.boats[_this.boats.length - 1];
-			}
+			for (i = y; i < y + length; i++) {
+				_this.grid[i][x] = _this.boats[_this.boats.length - 1];
+			}	
 		}
-
-		notifyObservers();
 	}
 
 	
@@ -90,11 +136,12 @@ var Boat = function(length, rotation) {
 	this.hits = 0;
 	this.isDestroyed = false;
 
-	this.hit() = function() {
+	this.hit = function() {
 		this.hits ++;
 
 		if (this.hits === this.length) {
 			this.isDestroyed = true;
+			console.log("boat sunk!");
 			console.log(this);
 		}
 	}
